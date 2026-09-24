de01164b80e4f2d8678e0c955f0ba5f08bc27d4b
jignacio17 <jleivaignacio07@gmail.com>
2026-09-24 10:47:21 -0500

    chore: setup backend y actualiza gitignore

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Save a Life API funcionando' })
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`))
